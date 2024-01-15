7908fc5b56550a302a315a82a4ee194d001258e9
gulshan <gulshanpandit123@gmail.com>
2024-01-16 00:25:45 +0530

    Add: 06 solution

---
+++ 01-Arrays-Hashing/06-Product-of-Array-Except-Self/productExceptSelf.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var productExceptSelf = function (nums) {
+  const res = Array.from({ length: nums.length }, () => 1);
+  let prefix = 1;
+  for (let i = 0; i < nums.length; i++) {
+    res[i] = prefix;
+    prefix *= nums[i];
+  }
+  let postfix = 1;
+  for (let i = nums.length - 1; i > -1; i--) {
+    res[i] *= postfix;
+    postfix *= nums[i];
+  }
+  return res;
+};

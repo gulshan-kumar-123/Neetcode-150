c08fe9df988ab575f8e32d1dc4a47d04f28357d6
gulshan-pandit-123 <gulshanpandit123@gmail.com>
2024-01-21 01:06:23 +0530

    Add: 14 solution

---
+++ 02-Two-Pointers/14-Trapping-Rain-Water/trap.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var trap = function (height) {
+  if (!height) {
+    return 0;
+  }
+  let l = 0;
+  let r = height.length - 1;
+  let leftMax = height[l];
+  let rightMax = height[r];
+  let res = 0;
+
+  while (l < r) {
+    if (height[l] < height[r]) {
+      l++;
+      leftMax = Math.max(leftMax, height[l]);
+      res += leftMax - height[l];
+    } else {
+      r--;
+      rightMax = Math.max(rightMax, height[r]);
+      res += rightMax - height[r];
+    }
+  }
+  return res;
+};

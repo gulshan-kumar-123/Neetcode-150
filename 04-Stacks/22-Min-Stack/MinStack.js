dd095a91df9fe566a84f09d8075d0921423aa891
gulshan-pandit-123 <gulshanpandit123@gmail.com>
2024-02-08 00:38:48 +0530

    Add: 22 solution

---
+++ 04-Stacks/22-Min-Stack/MinStack.js
@@ -0,0 +1,50 @@
+var MinStack = function () {
+  this.stack = [];
+  this.minStack = [];
+};
+
+/**
+ * @param {number} val
+ * @return {void}
+ */
+MinStack.prototype.push = function (val) {
+  this.stack.push(val);
+  if (this.minStack.length > 0) {
+    val2 = this.minStack[this.minStack.length - 1];
+  } else {
+    val2 = val;
+  }
+  val = Math.min(val, val2);
+  this.minStack.push(val);
+};
+
+/**
+ * @return {void}
+ */
+MinStack.prototype.pop = function () {
+  this.stack.pop();
+  this.minStack.pop();
+};
+
+/**
+ * @return {number}
+ */
+MinStack.prototype.top = function () {
+  return this.stack[this.stack.length - 1];
+};
+
+/**
+ * @return {number}
+ */
+MinStack.prototype.getMin = function () {
+  return this.minStack[this.minStack.length - 1];
+};
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * var obj = new MinStack()
+ * obj.push(val)
+ * obj.pop()
+ * var param_3 = obj.top()
+ * var param_4 = obj.getMin()
+ */
